34a2e7c963350667b576094ff1386b282ed4aff9
jrmelton50 <jrmelton@crimson.ua.edu>
2018-04-16 21:32:24 -0500

    changed list component to class-based

--- chirper/src/List.jsx
+++ chirper/src/List.jsx
@@ -1,15 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-const List = (props) => {
-    let listItems = props.items.map((item) => {  
+export default class List extends Component {
+    render() {
+        let listItems = this.props.items.map((item) => {
+            return (
+                <li key={item.id}> {item.value} </li>
+            );
+        });
+
         return (
-            <li key={item.id}> {item.value} </li> 
+            <ul> {listItems} </ul>
         );
-    });
-
-    return (
-        <ul> {listItems} </ul>
-    );
+    }
 }
-
-export default List;
